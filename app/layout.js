99bea12f8fe8668e2960ca060dfb763100ef946a
monazzamkhan1 <monazzamkhan@gmail.com>
2026-09-13 18:40:59 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Al Wahab Enterprises",
+  description: "Pharmaceutical Distribution Management System",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
