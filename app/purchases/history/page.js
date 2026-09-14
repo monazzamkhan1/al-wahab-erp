3c0f5f7edacac4808735789c177adcd60cbfa0a0
monazzamkhan1 <monazzamkhan@gmail.com>
2026-09-14 11:02:17 +0500

    Update page.js

--- app/purchases/history/page.js
+++ app/purchases/history/page.js
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../../supabase";
+import { supabase } from "../../../supabase";
 
 export default function PurchaseHistoryPage() {
   const [purchases, setPurchases] = useState([]);
